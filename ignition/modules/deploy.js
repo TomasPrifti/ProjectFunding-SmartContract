958b0014a124aa237abd4b95f71cfb86e71fbac5
Tomas Prifti <tomasprifti99@gmail.com>
2024-12-21 19:47:37 +0100

    feat(deploy, test): Modificato script per il deploy e implementato test per il costruttore

--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -2,12 +2,22 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("ProjectModule", (module) => {
+	// Defining constructor's parameters for the Contract.
+	const args = {
+		name: "Name of the project",
+		description: "Description of the project",
+		expiration: "Expiration of the project",
+		goal: 1000,
+		minCapital: 10,
+	};
+
+	// Deploying Contract.
 	const project = module.contract("Project", [
-		"Name of the project",
-		"Description of the project",
-		"Expiration of the project",
-		1000,
-		10,
+		args.name,
+		args.description,
+		args.expiration,
+		args.goal,
+		args.minCapital,
 	]);
 
 	return { project };
--- test/Project.js
+++ test/Project.js
@@ -1,3 +1,52 @@
-describe("Project", function () {
+const {
+	loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
+describe("Project", () => {
+
+	/**
+	 * Function used to deploy a new Contract.
+	 * 
+	 * @returns {Object}
+	 */
+	async function deployProjectFixture() {
+		// Contracts are deployed using the first signer/account by default.
+		const [owner, otherAccount] = await ethers.getSigners();
+
+		// Defining constructor's parameters for the Contract.
+		const args = {
+			name: "Name of the project",
+			description: "Description of the project",
+			expiration: "Expiration of the project",
+			goal: 1000,
+			minCapital: 10,
+		};
+
+		// Deploying a new contract everytime.
+		const project = await ethers.deployContract("Project", [
+			args.name,
+			args.description,
+			args.expiration,
+			args.goal,
+			args.minCapital,
+		]);
+
+		return { project, args, owner };
+	}
+
+	describe("constructor", () => {
+		it("Initializes the Project correctly", async () => {
+			const { project, args, owner } = await loadFixture(deployProjectFixture);
+
+			expect(await project.getName()).to.equal(args.name);
+			expect(await project.getDescription()).to.equal(args.description);
+			expect(await project.getExpiration()).to.equal(args.expiration);
+			expect(await project.getGoal()).to.equal(args.goal);
+			expect(await project.getMinCapital()).to.equal(args.minCapital);
+			expect(await project.getTargetWallet()).to.equal(owner);
+			expect(await project.getStatus()).to.equal("Active");
+		});
+	});
 });
