88c8ab7ed7352d2bfeb0c2d688dc641a15ff7c6d
Tomas Prifti <tomasprifti99@gmail.com>
2025-01-14 20:06:34 +0100

    feat(script): gestione script per la scrittura di `address` e `abi` del contratto deployato

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+	solidity: "0.8.28",
+	defaultNetwork: "hardhat",
+	networks: {
+		hardhat: {
+			chainId: 31337,
+		},
+		localhost: {
+			chainId: 31337,
+		},
+		sepolia: {
+			url: process.env.SEPOLIA_RPC_URL,
+			accounts: process.env.PRIVATE_KEY !== '' ? [process.env.PRIVATE_KEY] : [],
+			saveDeployments: true,
+			chainId: 11155111,
+		},
+		mainnet: {
+			url: process.env.MAINNET_RPC_URL,
+			accounts: process.env.PRIVATE_KEY !== '' ? [process.env.PRIVATE_KEY] : [],
+			saveDeployments: true,
+			chainId: 1,
+		},
+	},
 };
---
+++ scripts/update-front-end.js
@@ -0,0 +1,40 @@
+const { ethers, network } = require("hardhat");
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+dotenv.config();
+main().catch(error => console.error(error));
+
+/* Main Function */
+
+async function main() {
+	if (!process.env.UPDATE_FRONT_END || process.env.FRONT_END_ADDRESSES_FILE === '' || process.env.FRONT_END_ABI_FILE === '') {
+		console.error("Cannot update Front-End...");
+		return;
+	}
+
+	console.log("Updating Front-End...");
+
+	const manager = await ethers.getContractFactory("Manager");
+	const chainId = network.config.chainId;
+
+	// Updating Addresses File.
+	console.log("\nUpdating Addresses File...");
+	const currentAddresses = JSON.parse(fs.readFileSync(process.env.FRONT_END_ADDRESSES_FILE, "utf8"));
+
+	console.log("Writing on file...");
+	const contractAddressInfo = JSON.parse(fs.readFileSync(`ignition/deployments/chain-${chainId}/deployed_addresses.json`, "utf8"));
+	const contractAddress = contractAddressInfo["ManagerModule#Manager"] ?? '';
+	currentAddresses[chainId] = contractAddress;
+	fs.writeFileSync(process.env.FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses));
+	console.log("Writing successfully!");
+
+	// Updating ABI File.
+	console.log("\nUpdating ABI File...");
+	const currentABIs = JSON.parse(fs.readFileSync(process.env.FRONT_END_ABI_FILE, "utf8"));
+	
+	console.log("Writing on file...");
+	currentABIs[chainId] = manager.interface.fragments;
+	fs.writeFileSync(process.env.FRONT_END_ABI_FILE, JSON.stringify(currentABIs));
+	console.log("Writing successfully!");
+};
