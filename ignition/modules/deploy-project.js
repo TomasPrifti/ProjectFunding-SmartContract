7cca73b45abe4ccec310af0d703ccd21214012b1
Tomas Prifti <tomasprifti99@gmail.com>
2025-03-28 17:15:17 +0100

    feat(deploy-project): modificato script per il deploy del contratto `Project`

--- ignition/modules/deploy-project.js
+++ ignition/modules/deploy-project.js
@@ -1,26 +1,25 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const { network } = require("hardhat");
 
 // This deploy script can be used to deploy a single Project contract.
 module.exports = buildModule("ProjectModule", (module) => {
+	const chainId = network.config.chainId;
+
 	// Defining constructor's parameters for the Contract.
 	const args = {
 		name: "Name of the project",
 		description: "Description of the project",
-		expiration: 60 * 60 * 24 * 30, // 30 days.
-		goal: 10_000 * 10 ** 6, // 10.000 USDT.
 		minCapital: 100 * 10 ** 6, // 100 USDT.
-		targetWallet: "0x0000000000000000000000000000000000000000",
-		usdtToken: module.getParameter("MockUSDT"),
+		owner: "0x0000000000000000000000000000000000000000",
+		usdtToken: module.getParameter(`${chainId}-USDT`),
 	};
 
 	// Deploying Contract.
 	const project = module.contract("Project", [
 		args.name,
 		args.description,
-		args.expiration,
-		args.goal,
 		args.minCapital,
-		args.targetWallet,
+		args.owner,
 		args.usdtToken,
 	]);
 
